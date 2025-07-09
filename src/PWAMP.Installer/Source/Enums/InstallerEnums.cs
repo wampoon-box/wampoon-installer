7c9b59d98f85b119a99c21f72ef8584542e58052
frostybee <rsleiman2@gmail.com>
2025-07-09 19:03:42 -0400

    Refactor events

---
+++ src/PWAMP.Installer/Source/Enums/InstallerEnums.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wampoon.Installer.Enums
+{
+    internal class InstallerEnums
+    {
+    }
+}
